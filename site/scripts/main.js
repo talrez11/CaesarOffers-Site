45a402895852a19b1ea2e524518dc43a2cc1f5bf
Eyal Gershon <eyal@way2cu.com>
2015-08-13 11:40:15 +0300

    Added Redirection after form submission and pre selected option.

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -50,6 +50,9 @@ Site.is_mobile = function() {
  */
 Site.on_load = function() {
 
+	//Pre select UK
+	$("option[value='220']").attr('selected','selected');
+
 	function fading() {
 		var offerHeading = $('div.table_wrap h2');
 		offerHeading
@@ -58,6 +61,30 @@ Site.on_load = function() {
 	}
 
 	setInterval(fading, 1000);
+
+	//Redirect users to 2nd step
+		$('form').on('dialog-show', function(success) {
+			if (success) {
+				// get data from server and redirect
+				new Communicator('offers')
+					.on_success(function(lead_id) {
+						// register Google AdWords conversion
+						var google_conversion_id = 962137208;
+						var google_conversion_label = "rJh8COPexlgQ-JjkygM";
+						image = new Image(1,1);
+
+						image.addEventListener('load', function() {
+							window.location.replace('http://my.caesartrade.com/AccountSignup/API?liveLeadID='+lead_id);
+						});
+
+						image.src = 'http://www.googleadservices.com/pagead/conversion/' + google_conversion_id + '/?label=' + google_conversion_label + '&script=0&guid=ON';
+					})
+					.get('json_get_account_id');
+
+				// prevent dialog from showing
+				return false;
+			}
+		});
 
 };
 
